dc4c5eeca08e3d49decfab81cdb401f691f71250
link3z <info@recompila.com>
2014-12-03 20:56:18 +0100

    Añadido nuevo control rPosición

---
+++ 2.Base del programa/Controles/rPosicion.designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class rPosicion
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitulo = New ComponentFactory.Krypton.Toolkit.KryptonWrapLabel()
+        Me.lblNumero = New ComponentFactory.Krypton.Toolkit.KryptonWrapLabel()
+        Me.SuspendLayout()
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.AutoSize = False
+        Me.lblTitulo.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.lblTitulo.Location = New System.Drawing.Point(50, 0)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(110, 35)
+        Me.lblTitulo.StateCommon.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitulo.StateCommon.TextColor = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.lblTitulo.Text = "Tlitulo"
+        Me.lblTitulo.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'lblNumero
+        '
+        Me.lblNumero.AutoSize = False
+        Me.lblNumero.Font = New System.Drawing.Font("Arial", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumero.ForeColor = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.lblNumero.Location = New System.Drawing.Point(12, 28)
+        Me.lblNumero.Name = "lblNumero"
+        Me.lblNumero.Size = New System.Drawing.Size(28, 35)
+        Me.lblNumero.StateCommon.Font = New System.Drawing.Font("Arial", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumero.StateCommon.TextColor = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.lblNumero.Text = "1"
+        Me.lblNumero.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'rPosicion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Transparent
+        Me.Controls.Add(Me.lblNumero)
+        Me.Controls.Add(Me.lblTitulo)
+        Me.Name = "rPosicion"
+        Me.Size = New System.Drawing.Size(160, 84)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblTitulo As ComponentFactory.Krypton.Toolkit.KryptonWrapLabel
+    Friend WithEvents lblNumero As ComponentFactory.Krypton.Toolkit.KryptonWrapLabel
+
+End Class
---
+++ 2.Base del programa/Controles/rPosicion.vb
@@ -0,0 +1,88 @@
+﻿Public Class rPosicion
+
+#Region " PROPIEDADES "
+    ''' <summary>
+    ''' Numero a mostrar en el control
+    ''' </summary>
+    Public Property Numero As String
+        Get
+            Return iNumero
+        End Get
+        Set(value As String)
+            iNumero = value
+            lblNumero.Text = iNumero
+        End Set
+    End Property
+    Private iNumero As String = "1"
+
+    ''' <summary>
+    ''' Título a mostrar en el control
+    ''' </summary>
+    Public Property Titulo As String
+        Get
+            Return iTitulo
+        End Get
+        Set(value As String)
+            iTitulo = value
+            lblTitulo.Text = iTitulo
+        End Set
+    End Property
+    Private iTitulo As String = "Título"
+
+    ''' <summary>
+    ''' Alineación del título
+    ''' </summary>
+    Public Property AlieancionTitulo As System.Drawing.ContentAlignment
+        Get
+            Return iAlineacionTitulo
+        End Get
+        Set(value As System.Drawing.ContentAlignment)
+            iAlineacionTitulo = value
+            lblTitulo.TextAlign = iAlineacionTitulo
+        End Set
+    End Property
+    Private iAlineacionTitulo As System.Drawing.ContentAlignment = ContentAlignment.BottomCenter
+
+    ''' <summary>
+    ''' Color del objeto
+    ''' </summary>
+    Public Property Color As Color
+        Get
+            Return iColor
+        End Get
+        Set(value As Color)
+            iColor = value
+            aplicarColor()
+        End Set
+    End Property
+    Private iColor As Color = Color.FromArgb(66, 66, 66)
+#End Region
+
+#Region " CONTROL GRAFICO "
+    ''' <summary>
+    ''' Aplica los colores a los objetos que componen el control
+    ''' </summary>
+    Private Sub aplicarColor()
+        lblTitulo.StateCommon.TextColor = iColor
+        lblNumero.StateCommon.TextColor = iColor
+        Invalidate()
+    End Sub
+
+    ''' <summary>
+    ''' Dibuja el control
+    ''' </summary>
+    Protected Overrides Sub OnPaint(e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        e.Graphics.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+
+        Dim lapiz As New Pen(iColor, 6)
+
+        e.Graphics.DrawEllipse(lapiz, 5, 25, 40, 40)
+        e.Graphics.DrawLine(lapiz, 45, 45, 160, 45)
+        e.Graphics.FillEllipse(New SolidBrush(iColor), 157, 41, 7, 7)
+
+        lapiz.Dispose()
+    End Sub
+#End Region
+End Class
